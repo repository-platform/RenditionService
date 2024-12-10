e32028b2b484baf4a38786c2b3f4eb81473816af
Himanshu Sati <hsati@adobe.com>
2024-12-10 09:19:21 +0530

    Initial commit

---
+++ src/main/java/com/engineering/platform/RenditionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.engineering.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenditionServiceApplication {
+
+public static void main(String[] args) {
+SpringApplication.run(RenditionServiceApplication.class, args);
+}
+}
